8600a1dcd59b160bbafdf654d1fe1c893d7bd2d4
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-07-18 10:34:14 +0600

    postman kaaj kore

--- src/main/java/com/example/textsummarizer/services/DemoService.java
+++ src/main/java/com/example/textsummarizer/services/DemoService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 @Service
 public class DemoService {
     public Optional<Payload> summarize(Payload request){
-        Payload payload = new Payload("Summarized Text");
+        Payload payload = new Payload("Summarized Text of '"+request.getText()+"'");
         return Optional.of(payload);
     }
 }
