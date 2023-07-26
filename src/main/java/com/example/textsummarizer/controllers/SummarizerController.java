af9695e0d0883669b56e0bbae22ebd6854c2a183
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-07-26 15:01:18 +0600

    Wrote tokenization system

--- src/main/java/com/example/textsummarizer/controllers/SummarizerController.java
+++ src/main/java/com/example/textsummarizer/controllers/SummarizerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,7 +19,7 @@ public class SummarizerController {
     @Autowired
     private DemoService demoService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<Optional<Payload>> summarize (@RequestBody Payload reqPayload){
         Optional<Payload> response = demoService.summarize(reqPayload);
 
--- src/main/java/com/example/textsummarizer/services/DemoService.java
+++ src/main/java/com/example/textsummarizer/services/DemoService.java
@@ -1,6 +1,7 @@
 package com.example.textsummarizer.services;
 
 import com.example.textsummarizer.models.Payload;
+import com.example.textsummarizer.summarizer.SummarizerStartingPoint;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -8,7 +9,11 @@ import java.util.Optional;
 @Service
 public class DemoService {
     public Optional<Payload> summarize(Payload request){
-        Payload payload = new Payload("Summarized Text of '"+request.getText()+"'");
+        String summarizedText = SummarizerStartingPoint
+                .getInstance()
+                .driver( request.getText() );
+
+        Payload payload = new Payload("Summarized Text of '"+summarizedText+"'");
         return Optional.of(payload);
     }
 }
