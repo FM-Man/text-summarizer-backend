0424a14d494b0714856db3c7e7a17ffff6089a78
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-09-19 04:24:48 +0600

    clustering done

---
+++ python-app/Clusterer.py
@@ -0,0 +1,28 @@
+from sklearn.cluster import SpectralClustering
+from sklearn.metrics import pairwise_distances
+from numpy import exp
+from math import ceil
+from collections import defaultdict
+
+def spectral_clustering(sent_vecors):
+    sigma = 10
+    vectors=[]
+    for (i,v) in sent_vecors:
+        vectors.append(v)
+    
+    affinity_matrix = exp(- pairwise_distances(vectors, squared=True) / ( sigma ** 2))
+
+    n_clusters = ceil(len(sent_vecors)/4)
+    
+    model = SpectralClustering(n_clusters=n_clusters, affinity='precomputed')
+    # Fit the model to the data
+    model.fit(affinity_matrix)
+
+    cluster_indices = defaultdict(list)
+    cluster_org_indices = defaultdict(list)
+    for idx, label in enumerate(model.labels_):
+        cluster_indices[label].append(idx)
+        (i,v) = sent_vecors[idx]
+        cluster_org_indices[label].append(i)
+
+    return cluster_org_indices
--- python-app/Service.py
+++ python-app/Service.py
@@ -1,13 +1,24 @@
 from Preprocessor import word_divider
 from DatasetReader import read_vector
 from WordVectorizer import vectorizer
+from Clusterer import spectral_clustering
 
 def getSummary(text):
     sentences,splited_sentences = word_divider(text)
     vector_space = read_vector()
     print(vector_space['বাংলা'])
     vectors_with_sentence_index = vectorizer(vector_space , splited_sentences)
-    for (i,v) in vectors_with_sentence_index:
-        print(f's-{i}: {sentences[i]}\n{v}')
+    # for (i,v) in vectors_with_sentence_index:
+    #     print(f's-{i}: {sentences[i]}\n{v}')
+    
     # print(words)
+    clustered_indeces = spectral_clustering(vectors_with_sentence_index)
+        # Print the cluster indices
+    for cluster_idx, indices in clustered_indeces.items():
+        print(f"Cluster {cluster_idx}: {indices}")
+        for index in indices:
+            print(sentences[index])
+        print('==============================================')
+    
+
     return text
