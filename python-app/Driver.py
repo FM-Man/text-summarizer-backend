fa4e91228143a09b4b73a80f2e6f78f3d8a6ef3f
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-09-21 19:38:37 +0600

    result compiled
    lololol

--- python-app/Driver.py
+++ python-app/Driver.py
@@ -28,4 +28,35 @@ for index in range(len(evaluation_dictionary['documents'])):
 with open('output.json', 'w') as filehandle:
     json.dump(results, filehandle)
 
+avg_result = {
+    "rouge-1":{
+        "r": 0.0,
+        "p": 0.0,
+        "f": 0.0
+    },
+    "rouge-2": {
+        "r": 0.0,
+        "p": 0.0,
+        "f": 0.0
+    },
+    "rouge-l": {
+        "r": 0.0,
+        "p": 0.0,
+        "f": 0.0
+    }
+}
+length = len(results.values())
+for result in results.values():
+    avg_result["rouge-1"]["r"] += (1/length) * result["rouge-1"]["r"]
+    avg_result["rouge-1"]["p"] += (1/length) * result["rouge-1"]["p"]
+    avg_result["rouge-1"]["f"] += (1/length) * result["rouge-1"]["f"]
+    avg_result["rouge-2"]["r"] += (1/length) * result["rouge-2"]["r"]
+    avg_result["rouge-2"]["p"] += (1/length) * result["rouge-2"]["p"]
+    avg_result["rouge-2"]["f"] += (1/length) * result["rouge-2"]["f"]
+    avg_result["rouge-l"]["r"] += (1/length) * result["rouge-l"]["r"]
+    avg_result["rouge-l"]["p"] += (1/length) * result["rouge-l"]["p"]
+    avg_result["rouge-l"]["f"] += (1/length) * result["rouge-l"]["f"]
 
+
+with open('avg_output.json', 'w') as filehandle:
+    json.dump(avg_result, filehandle)
