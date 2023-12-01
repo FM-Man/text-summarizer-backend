1b304fd304634ba7b85a69a87fc49e125ac3ecb4
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-12-01 16:59:14 +0600

    uploading new controller

--- python-app/Controller.py
+++ python-app/Controller.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel
-from Service import getSummary
+from Service import getSummary as old_summary
+from exp_modules.expService import getSummary as new_summary
 
 app = FastAPI()
 
@@ -11,7 +12,13 @@ class Message(BaseModel):
 def index():
     return {"name":"fahim morshed"}
 
-@app.post("/summarize")
+@app.post("/v1/summarize")
 def summarize(m:Message):
-    summarizedText = getSummary(m.text)
+    summarizedText = old_summary(m.text)
+    return {"text": summarizedText}
+
+
+@app.post("/v2/summarize")
+def summarize(m:Message):
+    summarizedText = new_summary(m.text)
     return {"text": summarizedText}
