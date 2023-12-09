138b1a23ca39193fc00653e405d4818e8c61f047
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-12-10 01:11:51 +0600

    fooking ell maan spl done

--- python-app/Controller.py
+++ python-app/Controller.py
@@ -3,9 +3,19 @@ from pydantic import BaseModel
 from Service import getSummary as old_summary
 from exp_modules.expService import get_summary_ranked_sigma as new_summary_ranked
 from exp_modules.expService import get_summary_unranked_sigma as new_summary_unranked
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow all origins, methods, and headers (you might want to restrict these in a production environment)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with your front-end URL(s) in production
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Message(BaseModel):
     text : str
@@ -17,16 +27,16 @@ def index():
 @app.post("/v1/summarize")
 def summarize(m:Message):
     summarizedText = old_summary(m.text)
-    return {"text": summarizedText}
+    return {"text": m.text,"summary": summarizedText}
 
 
 @app.post("/v2/summarize")
 def summarize(m:Message):
     summarizedText = new_summary_unranked(m.text,0.5)
-    return {"text": summarizedText}
+    return {"text": m.text,"summary": summarizedText}
 
 
 @app.post("/v3/summarize")
 def summarize(m:Message):
     summarizedText = new_summary_ranked(m.text,0.5)
-    return {"text": summarizedText}
+    return {"text": m.text,"summary": summarizedText}
