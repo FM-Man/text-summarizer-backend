b36b7b6e834516fed957972c358ad33b0d6834d4
Fahim Morshed <bsse1102@iit.du.ac.bd>
2023-12-28 01:21:04 +0600

    cleaned some more

--- refactored/Service.py
+++ refactored/Service.py
@@ -12,64 +12,79 @@ import numpy
 
 nltk.download('punkt')
 
-
-def euclidean_distance(vect1, vect2):
-    return numpy.linalg.norm(vect1 - vect2)
-
-
-def similarity(tuple1, tuple2, sigma):
-    distances = []
-    pos1, cluster1 = tuple1
-    pos2, cluster2 = tuple2
-
-    for vect1 in cluster1:
-        min_dist = float('inf')
-        for vect2 in cluster2:
-            dist = euclidean_distance(vect1, vect2)
-            min_dist = min(dist, min_dist)
-        distances.append(min_dist)
-
-    for vect1 in cluster2:
-        min_dist = float('inf')
-        for vect2 in cluster1:
-            dist = euclidean_distance(vect1, vect2)
-            min_dist = min(dist, min_dist)
-        distances.append(min_dist)
-
-    if len(distances) == 0:
-        return 0
-
-    average = sum(distances) / len(distances)
-
-    return exp(- average ** 2 / (2 * sigma ** 2))
+"""================================== step 1 =======================================
+This part preprocesses the  input document. The preprocessing involves at first
+tokenizing the input document and then removing the stop words. the tokenization is
+done using the 'custom_tokenizer' function and stop word removal using the 
+'stopword_removal' function.
+================================================================================="""
 
 
-def spectral_clustering(sent_vectors_position, sigma):
-    total_sentence = len(sent_vectors_position)
-    affinity_matrix = []
-    for i in range(0, total_sentence):
-        row = [0] * total_sentence
-        affinity_matrix.append(row)
+def preprocessor(text):
+    sentence_dividers = ['।', '|', '!', '\n', '?', ":"]
+    sentences = custom_tokenizer(text, sentence_dividers)  # each sentences as intact and in a list
 
-    for i in range(0, total_sentence):
-        for j in range(i + 1, total_sentence):
-            affinity_matrix[i][j] = affinity_matrix[j][i] = similarity(sent_vectors_position[i],
-                                                                       sent_vectors_position[j], sigma)
+    word_dividers = [' ', ',', '.', ';', '"', "'", '`', '(', ')', '[', ']', '-', '‘', '’‌', '%', '/', '\\']
+    words = []
+    for sentence in sentences:
+        words.append(custom_tokenizer(sentence, word_dividers))  # each word as a separate token now
 
-    cluster_number = ceil(total_sentence / 4)
+    preprocessed_word_list = stopword_removal(words)
 
-    if len(affinity_matrix) > 1:
-        model = SpectralClustering(n_clusters=cluster_number, affinity='precomputed')
-        model.fit(affinity_matrix)
+    # the returning variables are two list.
+    # sentences = [sent1, sent2, sent3, ...]
+    # preprocessed_word_list = [[word11, word12, ...], [word21, word22, ...], ...]
+    return sentences, preprocessed_word_list
 
-        cluster_indices = defaultdict(list)
-        for idx, label in enumerate(model.labels_):
-            cluster_indices[label].append(idx)
 
-        return cluster_indices
+def custom_tokenizer(text, dividers):
+    # idk what this regex part is doing. got it from stack overflow.
+    divider_pattern = '|'.join(map(re.escape, dividers))
+    tokenizer = RegexpTokenizer(f'[^{divider_pattern}]+|[{divider_pattern}]')
+    tokens = tokenizer.tokenize(text)
 
+    # removes the divider tokens and also strips them
+    processed_tokens = []
+    for token in tokens:
+        if token.strip() and token not in dividers:
+            processed_tokens.append(token.strip())
+
+    return processed_tokens
+
+
+def stopword_removal(sentences):
+    stop_words = open("datasets\\stopwords.txt", "r", encoding="utf8").readlines()
+    for stop_word in stop_words:
+        stop_words.append(stop_word.split('\n')[0])
+
+    preprocessed_word_list = []
+    for sentence in sentences:
+        preprocessed_word_list.append([word for word in sentence if word not in stop_words])
+
+    return preprocessed_word_list
+
+
+"""====================================step 2=======================================
+Now the vector field consisting of over 1.4M bengali words are read. This function 
+is cached to save time in future. 'pydic.dic' is a python dictionary written in
+binary to save time reading. 'read_vectors_from_object_file' is used to read from
+this file. 'cc.bn.300.vec' is a plain text file. 'read_vectors_from_plaintext' is 
+used to read from here.
+================================================================================="""
+
+
+@lru_cache(maxsize=1)
+def read_vector():
+    if not os.path.exists('datasets\\pydic.dic'):
+        if not os.path.exists('datasets\\cc.bn.300.vec'):
+            print('dataset doesnot exist.'
+                  'download from https://drive.google.com/file/d/1qSjqzygv8_T7LC_S21nCvv_x_Rt-BkK5/view?usp=sharing')
+        else:
+            data = read_vectors_from_plaintext('datasets\\cc.bn.300.vec')
+            return data
     else:
-        return {}
+        data = read_vectors_from_object_file()
+        return data
 
 
 def read_vectors_from_plaintext(vec_file_path):
@@ -92,66 +107,13 @@ def read_vectors_from_object_file():
     return data
 
 
-def read_vector():
-    if not os.path.exists('datasets\\pydic.dic'):
-        if not os.path.exists('datasets\\cc.bn.300.vec'):
-            print(
-                'dataset doesnot exist. download from https://drive.google.com/file/d/1qSjqzygv8_T7LC_S21nCvv_x_Rt'
-                '-BkK5/view?usp=sharing')
-        else:
-            data = read_vectors_from_plaintext('datasets\\cc.bn.300.vec')
-            return data
-    else:
-        data = read_vectors_from_object_file()
-        return data
-
-
-# caching the dataset reading
-@lru_cache(maxsize=1)
-def get_resource():
-    return read_vector()
-
-
-def split_text_by_dividers(text, dividers):
-    divider_pattern = '|'.join(map(re.escape, dividers))
-
-    tokenizer = RegexpTokenizer(f'[^{divider_pattern}]+|[{divider_pattern}]')
-    sentences = tokenizer.tokenize(text)
-
-    sentences = [sentence.strip() for sentence in sentences if sentence.strip() and sentence not in dividers]
-
-    return sentences
-
-
-def stopword_removal(words):
-    stop_word_file = open("datasets\\stopwords.txt", "r", encoding="utf8")
-    stop_words = stop_word_file.readlines()
-    stop_words = [stop_word.split('\n')[0] for stop_word in stop_words]
-
-    preprocessed_word_list = []
-    for sentence in words:
-        preprocessed_word_list.append([word for word in sentence if word not in stop_words])
-
-    return preprocessed_word_list
-
-
-def word_divider(text):
-    sentence_dividers = ['।', '|', '!', '\n', '?', ":"]
-    sentences = split_text_by_dividers(text=text, dividers=sentence_dividers)
-
-    word_dividers = [' ', ',', '.', ';', '"', "'", '`', '(', ')', '[', ']', '-', '‘', '’‌', '%', '/', '\\']
-    words = []
-    for sentence in sentences:
-        words.append(split_text_by_dividers(sentence, word_dividers))
-
-    preprocessed_word_list = stopword_removal(words)
-    return sentences, preprocessed_word_list
+"""====================================step 3================================================
+replaces all the words with corresponding vectors
+=========================================================================================="""
 
 
 def vectorizer(dataset, sentences):
-    vectors = []
-    sentence_index = 0
-    total_sentence = len(sentences)
+    set_of_vectors = []
 
     for sentence in sentences:
         vectors_of_words_in_sentence = []
@@ -161,32 +123,103 @@ def vectorizer(dataset, sentences):
                 vectors_of_words_in_sentence.append(vector)
             except KeyError:
                 pass
-        sentence_index += 1
-        vectors.append((sentence_index / total_sentence, vectors_of_words_in_sentence))
+        set_of_vectors.append(vectors_of_words_in_sentence)
 
-    return vectors
+    # returns a 3d list
+    # [[vect1,vec2...],[vect3,vect4,...],...]
+    return set_of_vectors
 
 
+"""======================================step 4==============================================
+
+=========================================================================================="""
+
+
+def spectral_clustering(set_of_vectors, sigma):
+    # making an empty 2d array for an affinity matrix
+    total_sentence = len(set_of_vectors)
+    affinity_matrix = []
+    for i in range(0, total_sentence):
+        row = [0] * total_sentence
+        affinity_matrix.append(row)
+
+    # calculating similarity for affinity matrix
+    for i in range(0, total_sentence):
+        for j in range(i + 1, total_sentence):
+            affinity_matrix[i][j] = affinity_matrix[j][i] = (
+                similarity(set_of_vectors[i], set_of_vectors[j], sigma))
+
+    # number of clusters which is the size of the summary as the number of sentences
+    n_clusters = ceil(total_sentence / 4)
+
+    # clustering
+    if len(affinity_matrix) > 1:
+        model = SpectralClustering(n_clusters=n_clusters, affinity='precomputed')
+        model.fit(affinity_matrix)
+
+        cluster_indices = defaultdict(list)
+        for idx, label in enumerate(model.labels_):
+            cluster_indices[label].append(idx)
+
+        return cluster_indices
+
+    else:
+        return {}
+
+
+def similarity(sentence_x, sentence_y, sigma):
+    most_similar_distances = []
+
+    # finding the distance of the closest word from sentence y for every word in sentence x
+    for word_x in sentence_x:
+        msd = float('inf')
+        for word_y in sentence_y:
+            dist = euclidean_distance(word_x, word_y)
+            msd = min(dist, msd)
+        most_similar_distances.append(msd)
+
+    # finding the distance of the closest word from sentence x for every word in sentence y
+    for word_y in sentence_y:
+        msd = float('inf')
+        for word_x in sentence_x:
+            dist = euclidean_distance(word_x, word_y)
+            msd = min(dist, msd)
+        most_similar_distances.append(msd)
+
+    # to avoid zero divide by chance
+    if len(most_similar_distances) == 0:
+        return 0
+
+    avg_msd = sum(most_similar_distances) / len(most_similar_distances)
+
+    # gaussian similarity
+    return exp(- avg_msd ** 2 / (2 * sigma ** 2))
+
+
+def euclidean_distance(vect1, vect2):
+    return numpy.linalg.norm(vect1 - vect2)
+
+
+# entry point
 def get_summary(text, sigma=2):
-    sentences, split_sentences = word_divider(text)
-    vector_space = get_resource()
+    sentences, split_sentences = preprocessor(text)
+    vector_space = read_vector()
+    set_of_vectors = vectorizer(vector_space, split_sentences)
 
-    vectors_with_position = vectorizer(vector_space, split_sentences)
+    set_of_clusters = spectral_clustering(set_of_vectors, sigma)
+    indices_in_summary = []
 
-    clustered_indices = spectral_clustering(vectors_with_position, sigma)
-    summary_indices = []
+    for cluster in set_of_clusters.items():
+        picked_index = cluster[0]
+        indices_in_summary.append(picked_index)
 
-    for cluster_idx, indices in clustered_indices.items():
-        picked_index = indices[0]
-        summary_indices.append(picked_index)
-
-    for i in range(len(summary_indices)):
-        for j in range(i + 1, len(summary_indices)):
-            if summary_indices[i] > summary_indices[j]:
-                summary_indices[i], summary_indices[j] = summary_indices[j], summary_indices[i]
+    for i in range(len(indices_in_summary)):
+        for j in range(i + 1, len(indices_in_summary)):
+            if indices_in_summary[i] > indices_in_summary[j]:
+                indices_in_summary[i], indices_in_summary[j] = indices_in_summary[j], indices_in_summary[i]
 
     summary = ''
-    for index in summary_indices:
+    for index in indices_in_summary:
         summary += sentences[index] + '। '
 
-    return summary_indices, summary
+    return indices_in_summary, summary
