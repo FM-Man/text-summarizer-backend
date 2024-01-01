4399bfb9332a0ca0d0638e79d6e24f84689c6da5
Fahim Morshed <bsse1102@iit.du.ac.bd>
2024-01-01 12:10:00 +0600

    pycharm edit

--- refactored/Controller.py
+++ refactored/Controller.py
@@ -28,3 +28,7 @@ def index():
 def summarize(m: Message):
     _, summarized_text = summary(m.text)
     return {"text": m.text, "summary": summarized_text}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
