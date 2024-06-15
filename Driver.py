ff5967e7bb2ed137b7e9d72e6bc862d1195bc7b7
Fahim Morshed <bsse1102@iit.du.ac.bd>
2024-06-16 00:00:39 +0600

    fuckthishit man

--- Driver.py
+++ Driver.py
@@ -1,12 +1,11 @@
 from rouge import Rouge
 import json
-from Service import get_summary
-
+from ServiceNew import get_summary
 
 documents_summaries = json.load(open("document_summaries.json", "r", encoding="utf-8"))
 rouge = Rouge()
-sigmas=[9.0,10.0]
-resultComp=open("resultcomp.txt","a+",encoding="utf-8")
+sigmas = [0.0000000025,0.000000005,0.0000000075,0.000000025,0.00000005,0.000000075, 0.00000025,0.0000005,0.00000075]
+resultComp = open("resultcomp.csv", "a+", encoding="utf-8")
 
 for sig in sigmas:
     fahim = []
@@ -16,57 +15,57 @@ for sig in sigmas:
         row = {}
 
         document = documents_summaries[serial_no]["document"]
-        row["document"]=document
-        row["summary"]=documents_summaries[serial_no]['summary-1']
+        row["document"] = document
+        row["summary"] = documents_summaries[serial_no]['summary-1']
         # print(document)
 
-        _,machine_summary = get_summary(document,size=0.25,sigma=sig)
-        row["machine-summary"]=machine_summary
+        _, machine_summary = get_summary(document, size=0.25, sigma=sig)
+        row["machine-summary"] = machine_summary
         # print(machine_summary)
         # print(documents_summaries[serial_no]['summary-1'])
 
         rouge_score = rouge.get_scores(machine_summary, documents_summaries[serial_no]['summary-1'])[0]
-        row["rouge-1-r"]=rouge_score['rouge-1']['r']
-        row["rouge-1-p"]=rouge_score['rouge-1']['p']
-        row["rouge-1-f"]=rouge_score['rouge-1']['f']
-        row["rouge-2-r"]=rouge_score['rouge-2']['r']
-        row["rouge-2-p"]=rouge_score['rouge-2']['p']
-        row["rouge-2-f"]=rouge_score['rouge-2']['f']
-        row["rouge-l-r"]=rouge_score['rouge-l']['r']
-        row["rouge-l-p"]=rouge_score['rouge-l']['p']
-        row["rouge-l-f"]=rouge_score['rouge-l']['f']
-
+        row["rouge-1-r"] = rouge_score['rouge-1']['r']
+        row["rouge-1-p"] = rouge_score['rouge-1']['p']
+        row["rouge-1-f"] = rouge_score['rouge-1']['f']
+        row["rouge-2-r"] = rouge_score['rouge-2']['r']
+        row["rouge-2-p"] = rouge_score['rouge-2']['p']
+        row["rouge-2-f"] = rouge_score['rouge-2']['f']
+        row["rouge-l-r"] = rouge_score['rouge-l']['r']
+        row["rouge-l-p"] = rouge_score['rouge-l']['p']
+        row["rouge-l-f"] = rouge_score['rouge-l']['f']
+        print(rouge_score["rouge-1"]["f"], " ", rouge_score["rouge-2"]["f"], " ", rouge_score["rouge-l"]["f"])
         fahim.append(row)
 
         row = {}
         row["document"] = document
-        row["summary"]=documents_summaries[serial_no]['summary-2']
-        _,machine_summary = get_summary(document,size=.25,sigma=sig)
-        row["machine-summary"]=machine_summary
+        row["summary"] = documents_summaries[serial_no]['summary-2']
+        _, machine_summary = get_summary(document, size=.25, sigma=sig)
+        row["machine-summary"] = machine_summary
 
         rouge_score = rouge.get_scores(machine_summary, documents_summaries[serial_no]['summary-2'])[0]
-        row["rouge-1-r"]=rouge_score['rouge-1']['r']
-        row["rouge-1-p"]=rouge_score['rouge-1']['p']
-        row["rouge-1-f"]=rouge_score['rouge-1']['f']
-        row["rouge-2-r"]=rouge_score['rouge-2']['r']
-        row["rouge-2-p"]=rouge_score['rouge-2']['p']
-        row["rouge-2-f"]=rouge_score['rouge-2']['f']
-        row["rouge-l-r"]=rouge_score['rouge-l']['r']
-        row["rouge-l-p"]=rouge_score['rouge-l']['p']
-        row["rouge-l-f"]=rouge_score['rouge-l']['f']
-
+        row["rouge-1-r"] = rouge_score['rouge-1']['r']
+        row["rouge-1-p"] = rouge_score['rouge-1']['p']
+        row["rouge-1-f"] = rouge_score['rouge-1']['f']
+        row["rouge-2-r"] = rouge_score['rouge-2']['r']
+        row["rouge-2-p"] = rouge_score['rouge-2']['p']
+        row["rouge-2-f"] = rouge_score['rouge-2']['f']
+        row["rouge-l-r"] = rouge_score['rouge-l']['r']
+        row["rouge-l-p"] = rouge_score['rouge-l']['p']
+        row["rouge-l-f"] = rouge_score['rouge-l']['f']
+        print(rouge_score["rouge-1"]["f"], " ", rouge_score["rouge-2"]["f"], " ", rouge_score["rouge-l"]["f"])
         fahim.append(row)
         # print(rouge_score)
 
-    sum_r1_p=0
+    sum_r1_p = 0
     sum_r1_r = 0
-    sum_r1_f=0
-    sum_r2_p=0
+    sum_r1_f = 0
+    sum_r2_p = 0
     sum_r2_r = 0
-    sum_r2_f=0
-    sum_rl_p=0
+    sum_r2_f = 0
+    sum_rl_p = 0
     sum_rl_r = 0
-    sum_rl_f=0
+    sum_rl_f = 0
 
     for row in fahim:
         sum_r1_p += row["rouge-1-p"]
@@ -79,8 +78,12 @@ for sig in sigmas:
         sum_rl_r += row["rouge-l-r"]
         sum_rl_f += row["rouge-l-f"]
 
-    fahim.append([sum_r1_r/500,sum_r1_p/500,sum_r1_f/500,sum_r2_r/500,sum_r2_p/500,sum_r2_f/500,sum_rl_r/500,sum_rl_p/500,sum_rl_f/500])
-    resultComp.write(str(sig)+" : "+str(sum_r1_r/500)+" - "+str(sum_r1_p/500)+" - "+str(sum_r1_f/500)+" - "+str(sum_r2_r/500)+" - "+str(sum_r2_p/500)+" - "+str(sum_r2_f/500)+" - "+str(sum_rl_r/500)+" - "+str(sum_rl_p/500)+" - "+str(sum_rl_f/500))
-    json.dump(fahim, open("fahimResult"+str(sig)+".json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
+    fahim.append(
+        [sum_r1_r / 500, sum_r1_p / 500, sum_r1_f / 500, sum_r2_r / 500, sum_r2_p / 500, sum_r2_f / 500, sum_rl_r / 500,
+         sum_rl_p / 500, sum_rl_f / 500])
+    resultComp.write(str(sig) + "," + str(sum_r1_r / 500) + "," + str(sum_r1_p / 500) + "," + str(
+        sum_r1_f / 500) + "," + str(sum_r2_r / 500) + "," + str(sum_r2_p / 500) + "," + str(
+        sum_r2_f / 500) + "," + str(sum_rl_r / 500) + "," + str(sum_rl_p / 500) + "," + str(sum_rl_f / 500)+",0.441516747,0.490588947,0.381699937,0.445846941\n")
+    json.dump(fahim, open("fahimResult" + str(sig) + ".json", "w", encoding="utf-8"), ensure_ascii=False, indent=4)
 
 resultComp.close()
